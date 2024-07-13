c46db0c828f9dd37fbbe389a32f9011f2ee517d1
Even-dev <samuca1234.oficial@gmail.com>
2024-07-13 15:56:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// redirecionando usuário para youtube
+document.querySelector('img').addEventListener('click',function ret() {
+  
+  window.location.href="https://www.youtube.com/live/jfKfPfyJRdk?si=U3yXsfswmsdzfmih"
+})
